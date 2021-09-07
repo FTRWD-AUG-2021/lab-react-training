8aa1fd0b21168c18604ae3c4134d8280ca1b74d9
jbaker825 <jbaker825@gmail.com>
2021-09-07 16:38:19 -0500

    iteration 4

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import IdCard from './components/IdCard';
 import Greetings from './components/Greetings';
 import Random from './components/Random.js'
+import BoxColor from './components/BoxColor.js'
+
 function App() {
   return (
     <div className="App">
@@ -29,6 +31,9 @@ function App() {
 
       <Random min={1} max={6} />
       <Random min={1} max={100} />
+
+      <BoxColor r={255} g={0} b={0} />
+      <BoxColor r={128} g={255} b={0} />
     </div>
   );
 }
